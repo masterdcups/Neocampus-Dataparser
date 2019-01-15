1fd7431ec44efabcc7c95443d42e89612e924872
Johan <elmander46@gmail.com>
2019-01-15 14:26:04 +0100

    Ajout de la séparation en fichiers selon le subId
    Code de pierre / séparation en 6 fichiers

---
+++ separation.py
@@ -0,0 +1,35 @@
+import csv
+import annotationUnique
+
+def parcourirFichier(fichierE,fichierS):
+	
+	tableauId = [ "ilot1","ilot2","ilot3","ouest","57","79" ]
+	
+	for value in tableauId
+		nomFichier = "fichier" + value + ".csv"
+		fichier = open(nomFichier,"w")
+		with open(fichierE,encoding='utf-8') as f:
+	 
+			reader = csv.reader(f,delimiter=';')
+        
+			num_ligne = 0
+
+			for line in reader:
+				if num_ligne == 0:
+					new_line = line[0] + ";" + line[1] + ";" + line[2] + ";" + line[3] + ";" + line[4] + ";" + line[5] + "\n"
+				else:
+					if line[5] == value:
+						new_line = line[0] + ";" + line[1] + ";" + line[2] + ";" + line[3] + ";" + line[4] + ";" + line[5] + "\n"
+						
+					  
+										
+					
+					
+				fichier.write(new_line)				
+
+		fichier.close()
+
+    
+
+lireFichier("C:/Users/Bastien/Desktop/ProjetM2/dataBrut.csv","C:/Users/Bastien/Desktop/ProjetM2/")
+
