6c24be9252a50c88d5eb7e4fb8447f36df270d66
Johan <elmander46@gmail.com>
2019-01-28 09:00:56 +0100

    Modification échelles de 0 à 5
    Annotation lignes uniques

--- annotationUnique.py
+++ annotationUnique.py
@@ -6,32 +6,60 @@ Ceci est un script temporaire.
 """
 
 def temperature(valeur):
-    if(valeur < 24.2 and valeur > 19.8):
-        return 0
-    elif(valeur > 24.2):
+    if(valeur < 23.2 and valeur > 20.8):
+        return 5
+    elif(valeur < 24.5 and valeur > 19.5):
+        return 4
+    elif(valeur < 26 and valeur > 18):
+        return 3
+    elif(valeur < 27 and valeur > 17):
+        return 2
+    elif(valeur < 28 and valeur > 16):
         return 1
     else:
-        return -1
+        return 0
 
 def luminosite(valeur):
-    if(valeur > 500 and valeur < 1000):
-        return 0
-    elif(valeur < 500):
-        return -1
-    else:
+    if(valeur > 600 and valeur < 900):
+        return 5
+    elif(valeur > 550 and valeur < 950):
+        return 4
+    elif(valeur > 500 and valeur < 1000):
+        return 3
+    elif(valeur > 400 and valeur < 1100):
+        return 2
+    elif(valeur > 200 and valeur < 1300):
         return 1
+    else:
+        return 0
 
 def co2(valeur):
-    if(valeur<1200):
-        return 0
-    else:
+    if(valeur<400):
+        return 5
+    elif(valeur<600):
+        return 4
+    elif(valeur<800):
+        return 3
+    elif(valeur<1000):
+        return 2
+    elif(valeur<1200):
         return 1
+    else:
+        return 0
     
 def humidite(valeur):
-    if(valeur < 60 and valeur > 40):
-        return 0
-    else:
+    if(valeur < 55 and valeur > 45):
+        return 5
+    elif(valeur < 58 and valeur > 42):
+        return 4
+    elif(valeur < 63 and valeur > 37):
+        return 3
+    elif(valeur < 70 and valeur > 30):
+        return 2
+    elif(valeur < 80 and valeur > 20):
         return 1
+    else:
+        return 0
 
 def annotationCapteur(valeurTemperature, valeurLuminosite, valeurCo2, valeurHumidite):
     annotationRetour = ""
